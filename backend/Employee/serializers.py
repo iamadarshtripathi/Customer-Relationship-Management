2c23ebfb3966dd90bb73491e47033664db683eeb
adarsh29tripathi@gmail.com <adarsh29tripathi@gmail.com>
2024-04-22 12:39:33 +0530

    added mydb.py file for database , updated settings.py file , created Employees model in models.py file , add serializers.py file

---
+++ backend/Employee/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Employees
+
+class EmployeesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Employees
+        fields = ('empId' ,
+            'first_name' ,
+            'last_name' ,
+            'gender' ,
+            'contact_no' ,
+            'email' ,
+            'address' ,
+            'salary' ,
+            'department' )
--- backend/Employee/views.py
+++ backend/Employee/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import EmployeesSerializer
+from django.http.response import JsonResponse
 
-# Create your views here.
+class EmployeesView(APIView):
+    
+    def post(self , request):
+        data = request.data
+        serializer = EmployeesSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse("Employee Created Successfully" , safe = False)
+        return JsonResponse("Failed to add Employee" , safe = False)
+
