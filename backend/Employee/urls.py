235eb84e911e8b4e5a767bd1ff48578e27ac16f8
adarsh29tripathi@gmail.com <adarsh29tripathi@gmail.com>
2024-04-23 17:15:11 +0530

    updated few files views.py and urls.py where i added apis functionality for get , put and delete for employees

--- backend/Employee/urls.py
+++ backend/Employee/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import EmployeesView
 
 urlpatterns = [
-    path('Employee/', EmployeesView.as_view())
+    path("Employee/", EmployeesView.as_view()),
+    path("Employee/<int:pk>/", EmployeesView.as_view())
 ]
--- backend/Employee/views.py
+++ backend/Employee/views.py
@@ -17,8 +17,31 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import EmployeesSerializer
+from .models import Employees
+from django.http.response import Http404
+from django.http.response import JsonResponse
 
 class EmployeesView(APIView):
+    
+    def get_Employees(self,pk):
+        try:
+            employees = Employees.objects.get(empId=pk)
+            return employees
+        except:
+            raise Response("Employee Does not Exist")
+        
+
+    def get(self, request, pk=None):
+        if pk:
+            data = self.get_Employees(pk)
+            serializer = EmployeesSerializer(data)   
+        else:
+            data = Employees.objects.all()
+            serializer = EmployeesSerializer(data, many=True)   
+        return Response(serializer.data)   
+
+
+
     def post(self, request):
         serializer = EmployeesSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,4 +49,21 @@ class EmployeesView(APIView):
             return Response("Employee Created Successfully", status=201)
         return Response(serializer.errors, status=400)
 
+
+
+    def put(self , request , pk=None):
+        employees_to_update = Employees.objects.get(empId=pk)
+        serializer = EmployeesSerializer(instance=employees_to_update, data=request.data, partial=True)   
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Employee updated Successfully", status=201)
+        return Response(serializer.errors, status=400)
+    
+
+
+    def delete(self, request , pk=None):
+        employee_to_delete = Employees.objects.get(empId=pk)
+        employee_to_delete.delete()
+        return Response("Employee deleted Successfully", status=201)
 
